ca40bf389e0811cb0e39f37b205bd4ac2be2d133
dharmik <dhrmkjdv@gmail.com>
2023-11-28 01:10:20 +0530

    initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,48 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        dhrmk: "url('/bg.png')",
+        dhrmkAstro: "url('/bg-astro.jpg')",
+      },
+      fontFamily: {
+        japan: ["'Noto Serif JP'", "serif"],
+      },
+      // Define animation class
+      animation: {
+        scrollForever: "scrollForever 30s linear infinite",
+        scrollReverse: "scrollReverse 30s linear infinite",
+        scrollForeverX: "scrollForeverX 30s linear infinite",
+        scrollReverseX: "scrollReverseX 30s linear infinite",
+      },
+      // Define keyframes
+      keyframes: {
+        scrollForever: {
+          "0%": { "background-position": "0 0" },
+          "100%": { "background-position": "0 -1356px" },
+        },
+        scrollReverse: {
+          "0%": { "background-position": "0 0" },
+          "100%": { "background-position": "0 1356px" },
+        },
+        scrollForeverX: {
+          "0%": { "background-position": "0 0" },
+          "100%": { "background-position": "-1356px 0" },
+        },
+        scrollReverseX: {
+          "0%": { "background-position": "0 0" },
+          "100%": { "background-position": "1356px 0" },
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
