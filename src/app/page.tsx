e433af6e18fd510bcc4ccbc74f1c7f032cf56b9a
dharmik <dhrmkjdv@gmail.com>
2023-11-28 13:46:56 +0530

    padding fix mobile dimensions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <main className="relative min-h-screen p-4 md:p-6 pb-0 ">
+    <main className="relative min-h-screen px-4 pt-4 md:px-6 md:pt-6 pb-0 ">
       <div className="absolute inset-0 min-h-screen bg-dhrmkAstro bg-repeat bg-contain animate-scrollForeverX -z-10"></div>
       <div className="max-w-[900px] mx-auto flex">
         {/* <div className="hidden md:flex group relative flex-col h-screen w-fit max-w-fit rounded-xl overflow-hidden mx-4">
