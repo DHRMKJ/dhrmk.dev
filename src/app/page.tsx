e77210f34ac32b4a4f626156ede44a844cd5a065
dharmik <dhrmkjdv@gmail.com>
2023-11-28 13:03:10 +0530

    fix svg sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,22 @@ export default function Home() {
     <main className="relative min-h-screen p-6 pb-0 ">
       <div className="absolute inset-0 min-h-screen bg-dhrmkAstro bg-repeat bg-contain animate-scrollForeverX -z-10"></div>
       <div className="max-w-[900px] mx-auto h-full flex">
+        {/* <div className="hidden md:flex group relative flex-col h-screen w-fit max-w-fit rounded-xl overflow-hidden mx-4">
+          <div className="absolute inset-0 h-full w-full bg-dhrmk bg-repeat-y bg-contain animate-scrollForever group-hover:animate-scrollReverse" />
+          <div className="flex z-10 h-1/2 items-center mx-auto">
+            <div
+              className="flex flex-col text-5xl group-hover:text-7xl text-center font-japan"
+              lang="zh-Hans"
+            >
+              {"分揺豆".split("").map((letter, index) => (
+                <p key={index}>{letter}</p>
+              ))}
+            </div>
+          </div>
+          <div className="flex items-end h-1/2  z-10">
+            <Image src="/dhrmk.png" height={150} width={250} alt="me"></Image>
+          </div>
+        </div> */}
         <div className="flex w-full flex-col space-y-4 lg:space-y-10 mx-auto">
           {/*first card*/}
           <div className="flex relative flex w-full rounded-xl overflow-hidden shadow-2 shadow-gray-50 cursor-rocket">
@@ -43,7 +59,7 @@ export default function Home() {
             </div>
             <div className="flex justify-between bg-[#6644cc] p-2 lg:px-4">
               <a href="https://github.com/dhrmkj">
-                <div className="m-2 h-7 w-7 hover:scale-125">
+                <div className="m-2 h-8 w-8 hover:scale-125">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="-8 0 512 512"
@@ -58,7 +74,7 @@ export default function Home() {
               </a>
               <a href="https://www.linkedin.com/in/dharmik-jadav-064421248/">
                 <div
-                  className="m-2 h-6 w-6 hover:scale-125"
+                  className="m-2 h-8 w-8 hover:scale-125"
                   aria-label="linkedIn"
                 >
                   <svg
@@ -74,7 +90,7 @@ export default function Home() {
               </a>
 
               <a href="mailto:dhrmkjdv@gmail.com">
-                <div className="m-2 h-6 w-6 hover:scale-125">
+                <div className="m-2 h-8 w-8 hover:scale-125">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 512 512"
@@ -89,7 +105,7 @@ export default function Home() {
                 </div>
               </a>
               <a href="https://discord.com/users/1103331985239322635">
-                <div className="m-2 h-6 w-6 hover:scale-125">
+                <div className="m-2 h-8 w-8 hover:scale-125">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 -64 640 640"
@@ -103,7 +119,7 @@ export default function Home() {
                 </div>
               </a>
               <a href="https://twitter.com/Lunch__enjoyer">
-                <div className="m-2 h-6 w-6 hover:scale-125">
+                <div className="m-2 h-8 w-8 hover:scale-125">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 512 512"
@@ -125,7 +141,7 @@ export default function Home() {
             </div>
 
             <div className="flex bg-[#2299bb] justify-between p-2 lg:px-4">
-              <div className="w-6 h-6 m-2 hover:scale-125">
+              <div className="w-8 h-8 m-2 hover:scale-125">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="-64 0 512 512"
@@ -138,7 +154,7 @@ export default function Home() {
                   <path d="M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z"></path>
                 </svg>
               </div>
-              <div className="w-6 h-6 m-2 hover:scale-125">
+              <div className="w-8 h-8 m-2 hover:scale-125">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="-64 0 512 512"
@@ -150,7 +166,7 @@ export default function Home() {
                   <path d="M0 32l34.9 395.8L192 480l157.1-52.2L384 32H0zm313.1 80l-4.8 47.3L193 208.6l-.3.1h111.5l-12.8 146.6-98.2 28.7-98.8-29.2-6.4-73.9h48.9l3.2 38.3 52.6 13.3 54.7-15.4 3.7-61.6-166.3-.5v-.1l-.2.1-3.6-46.3L193.1 162l6.5-2.7H76.7L70.9 112h242.2z"></path>
                 </svg>
               </div>
-              <div className="w-6 h-6 m-2 hover:scale-125">
+              <div className="w-8 h-8 m-2 hover:scale-125">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="-32 0 512 512"
@@ -162,7 +178,7 @@ export default function Home() {
                   <path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM243.8 381.4c0 43.6-25.6 63.5-62.9 63.5-33.7 0-53.2-17.4-63.2-38.5l34.3-20.7c6.6 11.7 12.6 21.6 27.1 21.6 13.8 0 22.6-5.4 22.6-26.5V237.7h42.1v143.7zm99.6 63.5c-39.1 0-64.4-18.6-76.7-43l34.3-19.8c9 14.7 20.8 25.6 41.5 25.6 17.4 0 28.6-8.7 28.6-20.8 0-14.4-11.4-19.5-30.7-28l-10.5-4.5c-30.4-12.9-50.5-29.2-50.5-63.5 0-31.6 24.1-55.6 61.6-55.6 26.8 0 46 9.3 59.8 33.7L368 290c-7.2-12.9-15-18-27.1-18-12.3 0-20.1 7.8-20.1 18 0 12.6 7.8 17.7 25.9 25.6l10.5 4.5c35.8 15.3 55.9 31 55.9 66.2 0 37.8-29.8 58.6-69.7 58.6z"></path>
                 </svg>
               </div>
-              <div className="w-6 h-6 m-2 hover:scale-125">
+              <div className="w-8 h-8 m-2 hover:scale-125">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="-32 0 512 512"
@@ -174,7 +190,7 @@ export default function Home() {
                   <path d="M224 508c-6.7 0-13.5-1.8-19.4-5.2l-61.7-36.5c-9.2-5.2-4.7-7-1.7-8 12.3-4.3 14.8-5.2 27.9-12.7 1.4-.8 3.2-.5 4.6.4l47.4 28.1c1.7 1 4.1 1 5.7 0l184.7-106.6c1.7-1 2.8-3 2.8-5V149.3c0-2.1-1.1-4-2.9-5.1L226.8 37.7c-1.7-1-4-1-5.7 0L36.6 144.3c-1.8 1-2.9 3-2.9 5.1v213.1c0 2 1.1 4 2.9 4.9l50.6 29.2c27.5 13.7 44.3-2.4 44.3-18.7V167.5c0-3 2.4-5.3 5.4-5.3h23.4c2.9 0 5.4 2.3 5.4 5.3V378c0 36.6-20 57.6-54.7 57.6-10.7 0-19.1 0-42.5-11.6l-48.4-27.9C8.1 389.2.7 376.3.7 362.4V149.3c0-13.8 7.4-26.8 19.4-33.7L204.6 9c11.7-6.6 27.2-6.6 38.8 0l184.7 106.7c12 6.9 19.4 19.8 19.4 33.7v213.1c0 13.8-7.4 26.7-19.4 33.7L243.4 502.8c-5.9 3.4-12.6 5.2-19.4 5.2zm149.1-210.1c0-39.9-27-50.5-83.7-58-57.4-7.6-63.2-11.5-63.2-24.9 0-11.1 4.9-25.9 47.4-25.9 37.9 0 51.9 8.2 57.7 33.8.5 2.4 2.7 4.2 5.2 4.2h24c1.5 0 2.9-.6 3.9-1.7s1.5-2.6 1.4-4.1c-3.7-44.1-33-64.6-92.2-64.6-52.7 0-84.1 22.2-84.1 59.5 0 40.4 31.3 51.6 81.8 56.6 60.5 5.9 65.2 14.8 65.2 26.7 0 20.6-16.6 29.4-55.5 29.4-48.9 0-59.6-12.3-63.2-36.6-.4-2.6-2.6-4.5-5.3-4.5h-23.9c-3 0-5.3 2.4-5.3 5.3 0 31.1 16.9 68.2 97.8 68.2 58.4-.1 92-23.2 92-63.4z"></path>
                 </svg>
               </div>
-              <div className="w-6 h-6 m-2 hover:scale-125">
+              <div className="w-8 h-8 m-2 hover:scale-125">
                 <svg
                   viewBox="0 0 32 32"
                   version="1.1"
@@ -202,7 +218,7 @@ export default function Home() {
                   />
                 </svg>
               </div>
-              <div className="w-6 h-6 m-2 hover:scale-125">
+              <div className="w-8 h-8 m-2 hover:scale-125">
                 <svg
                   fill="none"
                   viewBox="0 0 512 512"
@@ -217,7 +233,7 @@ export default function Home() {
                   />
                 </svg>
               </div>
-              <div className="w-6 h-6 m-2 hover:scale-125">
+              <div className="w-8 h-8 m-2 hover:scale-125">
                 <svg
                   viewBox="0 0 256 288"
                   xmlns="http://www.w3.org/2000/svg"
@@ -249,7 +265,7 @@ export default function Home() {
                   />
                 </svg>
               </div>
-              <div className="w-6 h-6 m-2 hover:scale-125">
+              <div className="w-8 h-8 m-2 hover:scale-125">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 512 512"
@@ -277,22 +293,6 @@ export default function Home() {
               </h4>
             </div>
           </div>
-          {/* <div className="hidden md:flex group relative flex-col h-screen w-fit max-w-fit rounded-xl overflow-hidden mx-4">
-          <div className="absolute inset-0 h-full w-full bg-dhrmk bg-repeat-y bg-contain animate-scrollForever group-hover:animate-scrollReverse" />
-          <div className="flex z-10 h-1/2 items-center mx-auto">
-            <div
-              className="flex flex-col text-5xl group-hover:text-7xl text-center font-japan"
-              lang="zh-Hans"
-            >
-              {"分揺豆".split("").map((letter, index) => (
-                <p key={index}>{letter}</p>
-              ))}
-            </div>
-          </div>
-          <div className="flex items-end h-1/2  z-10">
-            <Image src="/dhrmk.png" height={150} width={250} alt="me"></Image>
-          </div>
-        </div> */}
         </div>
       </div>
     </main>
